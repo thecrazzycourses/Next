6f27b37c21865c36589c597df64654b1ee654afb
rahul9582816659 <rahul9582816659@gmail.com>
2020-03-29 07:21:49 +0530

    api added

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
+import fetch from 'isomorphic-unfetch'
+import Layout from '../components/Layout/Layout'
 
 class About extends Component {
-  getInitialProps() {}
-
-  componentDidMount() {
-    fetch('https://api.github.com/users/thecrazzycourses')
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-      })
+  static async getInitialProps() {
+    const res = await fetch('https://api.github.com/users/thecrazzycourses')
+    const data = await res.json()
+    return { user: data }
   }
 
   render() {
-    return <div></div>
+    const { name, avatar_url } = this.props.user
+    return (
+      <Layout tittle="Home">
+        <h1>Fetching Github Api for : {name}</h1>
+        <img src={avatar_url} alt="Me" height="200px"></img>
+      </Layout>
+    )
   }
 }
 
