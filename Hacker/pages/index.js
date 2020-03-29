d003ef424f961a4f094ea705a97dcd8f033728b5
rahul9582816659 <rahul9582816659@gmail.com>
2020-03-29 16:00:27 +0530

    update

--- Hacker/pages/index.js
+++ Hacker/pages/index.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import _fetch from 'isomorphic-fetch'
+import Error from 'next/error'
+import StoryList from '../components/StoryList'
+import Layout from '../components/Layout'
 
 class Index extends Component {
+  static async getInitialProps() {
+    let stories
+
+    try {
+      const res = await _fetch('https://node-hnapi.herokuapp.com/news?page=1')
+      stories = await res.json()
+    } catch (error) {
+      console.log(error)
+      stories = []
+    }
+
+    return { stories }
+  }
+
   render() {
+    const { stories } = this.props
+    if (stories.length === 0) {
+      return <Error statusCode={503} />
+    }
     return (
-      <div>
-        <h1>Welcome to Hacker News</h1>
-      </div>
+      <Layout title="Hacker News" description="Hacker News Next App">
+        <StoryList stories={stories} />
+      </Layout>
     )
   }
 }
